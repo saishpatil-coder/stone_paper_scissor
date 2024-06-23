4f0254295eb04c6921254799bbe016ceeddc0019
Saish Patil <Saishpatil9322@gmail.com>
2024-06-23 09:41:37 +0530

    2nd edit

--- game.js
+++ game.js
@@ -3,9 +3,11 @@ let uCont = document.querySelector(".user");
 let cCont = document.querySelector(".code");
 let uImgs  = document.querySelectorAll(".user > .image");
 let cImgs  = document.querySelectorAll(".code > .image");
+let btn = document.querySelectorAll(".image");
 let arr = ["stone" , "paper" , "scissor"] ;
 let uScore = 0 ;
 let cScore = 0 ;
+let intervalId = null ;
 userslect();
 
 function userslect(){
@@ -19,36 +21,37 @@ for(let i = 0 ; i < 3 ; i++){
     uImgs[i].addEventListener("click",()=>{
         user =  uImgs[i].getAttribute("id") ;
         codeSelect(user);
+        
     })
 }
 
 function userWin(user , code){
     uScore++ ;
-    document.querySelector("h1").innerText = "Hurray ..  "+user+" beats "+code;
-    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector("h1").innerText = "You won ..  "+user+" beats "+code;
+    // document.querySelector("body").style.backgroundColor = "green";
     setTimeout(function(){
         document.querySelector("h1").innerText = "Let's Play";
-        document.querySelector("body").style.backgroundColor = "greenyellow";
-    },2000)
+        // document.querySelector("body").style.backgroundColor = "greenyellow";
+    },1000)
 }
 
 function codeWin(user , code){
     cScore++ ;
-    document.querySelector("h1").innerText = "Arrayy ..  "+code+" beats "+user;
-    document.querySelector("body").style.backgroundColor = "red";
+    document.querySelector("h1").innerText = "You lost ..  "+code+" beats "+user;
+    // document.querySelector("body").style.backgroundColor = "red";
     setTimeout(function(){
         document.querySelector("h1").innerText = "Let's Play";
         document.querySelector("body").style.backgroundColor = "greenyellow";
-    },2000)
+    },1000)
 }
 
 function check(user , code){
     if(user === code){
         document.querySelector("h1").innerText = "It's Tie ";
-        document.querySelector("body").style.backgroundColor = "white";
+        // document.querySelector("body").style.backgroundColor = "white";
         setTimeout(function(){
             document.querySelector("h1").innerText = "Let's Play ";
-            document.querySelector("body").style.backgroundColor = "greenyellow";
+            // document.querySelector("body").style.backgroundColor = "greenyellow";
         },400)
     }
     if(user === "stone" && code === "scissor"){
@@ -78,9 +81,10 @@ function codeSelect(user){
     setTimeout(function(){
         document.querySelector( ".code > #"+selectedImage).classList.remove("pressed");
         
-    },1000)
-    console.log(selectedImage +" selected by code");
+    },1500);
+
     check(user , selectedImage);
+    
     document.querySelector(".userScore").innerText = uScore ;
     document.querySelector(".codeScore").innerText = cScore ;
 }
